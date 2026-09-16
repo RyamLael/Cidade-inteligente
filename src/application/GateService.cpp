0f8d289ba800bf9216f74151897d6139226e030e
Ryam Lael <ryyamlael@gmail.com>
2026-09-16 18:35:15 -0300

    fix: fix gate web control

--- src/application/GateService.cpp
+++ src/application/GateService.cpp
@@ -1,8 +1,6 @@
 #include "application/GateService.h"
 
 #include "Config.h"
-#include "SystemState.h"
-#include "StateLock.h"
 
 #include "drivers/Servo.h"
 
@@ -11,9 +9,6 @@ namespace
     ServoDriver gateServo(
         PIN_SERVO
     );
-
-    bool currentGateState =
-        false;
 }
 
 bool GateService::begin()
@@ -25,84 +20,39 @@ bool GateService::begin()
 
     gateServo.close();
 
-    currentGateState =
+    _isOpen =
         false;
-
-    STATE_LOCK();
-
-    g_systemState.gateOpen =
-        false;
-
-    STATE_UNLOCK();
 
     return true;
 }
 
-void GateService::update()
+void GateService::open()
 {
-    bool manualGateControl;
-    bool gateOpen;
-
-    uint16_t gateLight;
-    uint16_t gateThreshold;
-
-    STATE_LOCK();
-
-    manualGateControl =
-        g_systemState.manualGateControl;
-
-    gateOpen =
-        g_systemState.gateOpen;
-
-    gateLight =
-        g_systemState.gateLight;
-
-    gateThreshold =
-        g_systemState.gateThreshold;
-
-    STATE_UNLOCK();
-
-    bool desiredGateState;
-
-    if (manualGateControl)
-    {
-        desiredGateState =
-            gateOpen;
-    }
-    else
-    {
-        desiredGateState =
-            gateLight <
-            gateThreshold;
-    }
-
-    if (
-        desiredGateState ==
-        currentGateState
-    )
+    if (_isOpen)
     {
         return;
     }
 
-    if (desiredGateState)
-    {
-        gateServo.open();
-    }
-    else
-    {
-        gateServo.close();
-    }
+    gateServo.open();
 
-    currentGateState =
-        desiredGateState;
-
-    STATE_LOCK();
-
-    g_systemState.gateOpen =
-        currentGateState;
-
-    g_systemState.webStateChanged =
+    _isOpen =
         true;
+}
 
-    STATE_UNLOCK();
+void GateService::close()
+{
+    if (!_isOpen)
+    {
+        return;
+    }
+
+    gateServo.close();
+
+    _isOpen =
+        false;
+}
+
+bool GateService::isOpen() const
+{
+    return _isOpen;
 }
